1b96979f3f5756d770a079bad99f8f1bbfb2e25e
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2022-01-04 21:44:52 +0800

    Add script for scraping followed users of input accounts

--- scrape_follows/main.py
+++ scrape_follows/main.py
@@ -6,10 +6,16 @@ load_dotenv()
 
 TWITTER_BEARER_TOKEN = os.environ.get('TWITTER_BEARER_TOKEN')
 MONGODB_URI = os.environ.get('MONGODB_URI')
-user_id = '1465764550003904520'
-r = requests.get(
-    f'https://api.twitter.com/2/users/{user_id}/following', headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
-
-print(r.json())
+MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE')
 
 mongo_client = MongoClient(MONGODB_URI)
+db = mongo_client[MONGODB_DATABASE]
+
+inputs = db.input
+for input in inputs.find():
+    account_id = input['account_id']
+    if account_id:
+        r = requests.get(
+            f'https://api.twitter.com/2/users/{account_id}/following?user.fields=created_at,entities,description',
+            headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+        print(r.json())
